684b733d435cc572e8134f574a6a884f9918a3ca
Subham-sahoo <subhamsahoo291995@gmail.com>
2019-11-26 22:16:01 +0530

    SIMPLE_JAVA

---
+++ Add.java
@@ -0,0 +1,14 @@
+
+public class Add {
+
+	public static void main(String[] args) {
+		int i = 12;
+		int j = 34;
+		int k = 56;
+		int result = i - j * k;
+
+		System.out.println(result);
+
+	}
+
+}
